1c52857956c8a5b7d4b4b1dd4e56df13fbd099dc
ekhassan <ek.hassan.07@email.com>
2025-07-22 23:02:19 +0500

    Refactor code structure for improved readability and maintainability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,8 +30,8 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-1/90 lg:backdrop-blur-sm 
-      ${openNavigation ? "bg-n-1" : "bg-n-1 backdrop-blur-sm"}`}
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 bg-white lg:bg-n-1/90 lg:backdrop-blur-sm 
+      ${openNavigation ? "bg-white" : "bg-white backdrop-blur-sm"}`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
